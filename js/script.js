0ef13283a287d48e0fd4164b4e9a8f859938e1a1
Eduard Pancov <123413225+Hetfielddd@users.noreply.github.com>
2024-04-11 02:03:13 +0300

    added forms script

--- js/script.js
+++ js/script.js
@@ -206,7 +206,18 @@ window.addEventListener('DOMContentLoaded', () => {
         );
     menuItem.render();
 
+    // Forms
 
+    const forms = document.querySelectorAll('form');
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+        });
+    }
     
 });
 
